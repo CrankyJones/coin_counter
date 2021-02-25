b11bbd406622c9fbb07a5a5680b5651755d723f6
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-25 11:48:05 -0800

    All our tests passing
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- lib/main.rb
+++ lib/main.rb
@@ -3,11 +3,18 @@ class Coins
   def initialize()
     @quarter_count = 0
     @dime_count = 0
+    @nickel_count = 0
+    @penny_count = 0
   end
 
   def coin_counter(change_purse)
     result = ''
-    purse = change_purse.to_f.round(2).to_s.gsub(/[[:punct:]]/, '').to_i
+    if (change_purse.match?(/./))
+      purse = change_purse.to_f * 100
+      purse = purse.to_i
+    else
+      purse = change_purse.gsub(/[[:punct:]]/, '').to_i
+    end
     while (purse >= 25)
       purse -= 25
       @quarter_count += 1
@@ -16,11 +23,25 @@ class Coins
       purse -= 10
       @dime_count += 1
     end
+    while (purse >= 5)
+      purse -= 5
+      @nickel_count += 1
+    end
+    while (purse >= 1)
+      purse -= 1
+      @penny_count += 1
+    end
     if (@quarter_count > 0)
       result.concat("#{@quarter_count} quarter(s) ")
     end
     if (@dime_count > 0)
-      result.concat("#{@dime_count} dime(s)")
+      result.concat("#{@dime_count} dime(s) ")
+    end
+    if (@nickel_count > 0)
+      result.concat("#{@nickel_count} nickel(s) ")
+    end
+    if (@penny_count > 0)
+      result.concat("#{@penny_count} penny(s) ")
     end
     result
   end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -9,7 +9,17 @@ describe('Coins#coin_counter') do
 
   it('providing $.35 will result in one quarter and one dime') do
     change_purse = Coins.new()
-    expect(change_purse.coin_counter('.35')).to(eq('1 quarter(s) 1 dime(s)'))
+    expect(change_purse.coin_counter('.35')).to(eq('1 quarter(s) 1 dime(s) '))
     end
+
+  it('providing $.40 will result in one quarter and one dime one nickel') do
+    change_purse = Coins.new()
+    expect(change_purse.coin_counter('.40')).to(eq('1 quarter(s) 1 dime(s) 1 nickel(s) '))
+    end
+
+    it('providing $.41 will result in one quarter and one dime one nickel one penny') do
+      change_purse = Coins.new()
+      expect(change_purse.coin_counter('.41')).to(eq('1 quarter(s) 1 dime(s) 1 nickel(s) 1 penny(s) '))
+      end
 
 end
