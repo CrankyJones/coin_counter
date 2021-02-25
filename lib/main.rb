bdef18ce7a1f7390b0f8689f86d4a7447d7aa7b4
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-25 15:08:47 -0800

    Do a serious refactor
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,9 @@
 class Coins
 
   def initialize()
-    @quarter_count = 0
-    @dime_count = 0
-    @nickel_count = 0
-    @penny_count = 0
+    @change_purse = {'quarter' => 0, 'dime' => 0, 'nickel' => 0, 'penny' => 0}
+    @denominations = {'quarter' => 25, 'dime' => 10, 'nickel' => 5, 'penny' => 1}
+    @counter = 0
   end
 
   def coin_counter(change_purse)
@@ -13,37 +12,57 @@ class Coins
       purse = change_purse.to_f * 100
       purse = purse.to_i
     else
-      purse = change_purse.gsub(/[[:punct:]]/, '').to_i
+      purse = change_purse.to_i
     end
-    while (purse >= 25)
-      purse -= 25
-      @quarter_count += 1
+
+    @denominations.each() do |key, value|
+      while purse >= @denominations[key]
+          purse -= @denominations[key]
+          @change_purse[key] += 1
+      end
     end
-    while (purse >= 10)
-      purse -= 10
-      @dime_count += 1
-    end
-    while (purse >= 5)
-      purse -= 5
-      @nickel_count += 1
-    end
-    while (purse >= 1)
-      purse -= 1
-      @penny_count += 1
-    end
-    if (@quarter_count > 0)
-      result.concat("#{@quarter_count} quarter(s) ")
-    end
-    if (@dime_count > 0)
-      result.concat("#{@dime_count} dime(s) ")
-    end
-    if (@nickel_count > 0)
-      result.concat("#{@nickel_count} nickel(s) ")
-    end
-    if (@penny_count > 0)
-      result.concat("#{@penny_count} penny(s) ")
+    
+    @change_purse.each() do |key, value|
+      if (@change_purse[key] > 0)
+        result.concat("#{@change_purse[key]} #{@change_purse.keys[@counter]}(s) ")
+      end
+      @counter +=1
     end
     result
   end
-
 end
+
+
+
+    #{@change_purse[key]}
+
+# while (purse >= 25)
+    #   purse -= 25
+    #   @quarter_count += 1
+    # end
+    # while (purse >= 10)
+    #   purse -= 10
+    #   @dime_count += 1
+    # end
+    # while (purse >= 5)
+    #   purse -= 5
+    #   @nickel_count += 1
+    # end
+    # while (purse >= 1)
+    #   purse -= 1
+    #   @penny_count += 1
+    # end
+
+    # if (@quarter_count > 0)
+    #   result.concat("#{@quarter_count} quarter(s) ")
+    # end
+    # if (@dime_count > 0)
+    #   result.concat("#{@dime_count} dime(s) ")
+    # end
+    # if (@nickel_count > 0)
+    #   result.concat("#{@nickel_count} nickel(s) ")
+    # end
+    # if (@penny_count > 0)
+    #   result.concat("#{@penny_count} penny(s) ")
+    # end
+#     result
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,23 +3,23 @@ require('main')
 
 describe('Coins#coin_counter') do
   it('providing $.25 will result in one quarter') do
-  change_purse = Coins.new()
-  expect(change_purse.coin_counter('.25')).to(eq('1 quarter(s) '))
+    change_purse = Coins.new()
+    expect(change_purse.coin_counter('.25')).to(eq('1 quarter(s) '))
   end
 
   it('providing $.35 will result in one quarter and one dime') do
     change_purse = Coins.new()
     expect(change_purse.coin_counter('.35')).to(eq('1 quarter(s) 1 dime(s) '))
-    end
+  end
 
   it('providing $.40 will result in one quarter and one dime one nickel') do
     change_purse = Coins.new()
     expect(change_purse.coin_counter('.40')).to(eq('1 quarter(s) 1 dime(s) 1 nickel(s) '))
-    end
+  end
 
     it('providing $.41 will result in one quarter and one dime one nickel one penny') do
       change_purse = Coins.new()
       expect(change_purse.coin_counter('.41')).to(eq('1 quarter(s) 1 dime(s) 1 nickel(s) 1 penny(s) '))
-      end
+    end
 
 end
