5327b5c36c80e353d1761d4e3717fa9a633ca2eb
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-25 10:29:49 -0800

    Test 1 good pass
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,21 @@
 class Coins
 
+  def initialize()
+    @quarter_count = 0
+  end
+
   def coin_counter(change_purse)
+    purse = change_purse.to_f.round(2)
+    purse = purse.to_s
+    purse = purse.gsub(/[[:punct:]]/, '')
+    purse = purse.to_i
+    while (purse >= 25)
+      purse -= 25
+      @quarter_count += 1
+    end
+    if (@quarter_count > 0)
+      result = "#{@quarter_count} quarter(s)"
+    end
   end
 
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,6 +4,6 @@ require('main')
 describe('Coins#coin_counter') do
   it('providing $.25 will result in one quarter') do
   change_purse = Coins.new()
-  expect(change_purse.coin_counter('.25')).to(eq('1 quarter'))
+  expect(change_purse.coin_counter('.25')).to(eq('1 quarter(s)'))
   end
 end
