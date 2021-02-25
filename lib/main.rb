1acca0efca5eb51327ec12571f382b62749096d8
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-25 10:43:41 -0800

    Test 2 good pass
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- lib/main.rb
+++ lib/main.rb
@@ -2,20 +2,27 @@ class Coins
 
   def initialize()
     @quarter_count = 0
+    @dime_count = 0
   end
 
   def coin_counter(change_purse)
-    purse = change_purse.to_f.round(2)
-    purse = purse.to_s
-    purse = purse.gsub(/[[:punct:]]/, '')
-    purse = purse.to_i
+    result = ''
+    purse = change_purse.to_f.round(2).to_s.gsub(/[[:punct:]]/, '').to_i
     while (purse >= 25)
       purse -= 25
       @quarter_count += 1
     end
-    if (@quarter_count > 0)
-      result = "#{@quarter_count} quarter(s)"
+    while (purse >= 10)
+      purse -= 10
+      @dime_count += 1
     end
+    if (@quarter_count > 0)
+      result.concat("#{@quarter_count} quarter(s) ")
+    end
+    if (@dime_count > 0)
+      result.concat("#{@dime_count} dime(s)")
+    end
+    result
   end
 
 end
